dbfa3d5bd42d57d83bc001bbc6a8cae0bd4dfb9a
unknown <mohamedali.bouzaiene@esprit.tn>
2020-12-22 11:07:37 +0100

    removed target blank

--- js/search.js
+++ js/search.js
@@ -2,8 +2,6 @@ let searchBox=document.getElementById("searchtxt");
 
 
 searchBox.addEventListener('keypress',(e)=>{
-    console.log(e.keyCode);
-    
     if(e.keyCode==13){
         setTimeout(()=>{searchBox.value=""},1000);
     }
