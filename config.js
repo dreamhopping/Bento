6e210de436411361c8615ae193142cd9673b0c9a
dreamhopping <133573601+dreamhopping@users.noreply.github.com>
2023-05-14 12:50:49 +0100

    config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '0a12c026cbbd45e776ad85a6e21b823b', // Write here your API Key
+	weatherKey: '60d7cdce0bf078058c0df9aab6d7f1cf', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '52.498613',
+	defaultLongitude: '-1.762040',
 
 	// Autochange
 	autoChangeTheme: true,
