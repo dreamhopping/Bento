08b78fe6853de00bfd62b54981e0d2eaccdd0857
dreamhopping <actuallynamje@gmail.com>
2023-05-14 13:40:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -84,9 +84,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
+			name: 'YouTube',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://youtube.com/',
 		},
 	],
 
